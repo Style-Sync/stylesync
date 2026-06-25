9099e3bb48cd4293a6a5e34b788fd0da3b851367
yerixx <yerixxz@gmail.com>
2026-06-25 09:13:40 +0900

    feat: #48 Unsplash API 클라이언트(unsplashFetch) 구현

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "image.tmdb.org" }, // TMDB 포스터/배경
+      { protocol: "https", hostname: "i.scdn.co" }, // Spotify 아티스트/앨범 (Premium 풀리면)
+      { protocol: "https", hostname: "images.unsplash.com" }, // Unsplash 패션 이미지
+    ],
+  },
+};
 
 export default nextConfig;
